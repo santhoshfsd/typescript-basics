9ce26749a7eca03edd99256c22adc580756afff2
santhosh raj <santhoshraj19v@gmail.com>
2021-04-11 18:30:28 +0530

    shorthand syntax

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 class Department {
-    name: string;
+    // name: string;
     private employees: string[] = [];
 
-    constructor(n: string) {
-        this.name = n;
-    }
+    constructor(private id: string, public name: string) {
+        // this.id = id;
+        // this.name = n;
+      }
+    
 
     describe(this: Department) {
-        console.log('Department: ' + this.name);
+        console.log(`Department (${this.id}): ${this.name}`);
     }
 
     addEmployee(employee: string) {
@@ -21,7 +23,8 @@ class Department {
     }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department('d1', 'Accounting');
+
 
 accounting.describe();
 accounting.addEmployee('Max');
