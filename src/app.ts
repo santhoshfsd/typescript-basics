9cc4f426e719c85f0e0aac610bc04c51ed8df7f6
santhosh raj <santhoshraj19v@gmail.com>
2021-04-12 08:56:13 +0530

    read the dom elements value and added action listener

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 // Code goes here!
 class ProjectInput {
+    
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleElement: HTMLInputElement;
+    descriptionElement: HTMLInputElement
+    ownerElement: HTMLInputElement
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
         this.hostElement = document.getElementById('app')! as HTMLDivElement
@@ -10,12 +14,28 @@ class ProjectInput {
         //DocumentFragment type
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild! as HTMLFormElement;
+        this.element.id = "user-input";
+        
+        this.titleElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.ownerElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.configure();
         this.attach();
     }
 
     private attach() {
         // afterbegin afterend  /before
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+
+    private submitHandler(event:Event) {
+        event.preventDefault();
+        console.log(this.titleElement.value);
+        
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
     }
 }
 
