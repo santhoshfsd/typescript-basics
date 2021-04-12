a8c9d63e31dbf55fca85f279d0128c6c2426c1bc
santhosh raj <santhoshraj19v@gmail.com>
2021-04-12 20:41:33 +0530

    render project items

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ type Listener<T> = (items: T[]) => void
 
 // State 
 class ProjectState extends State<Project>{
-     private projects: Project[] = []
+    private projects: Project[] = []
     private static instance: ProjectState;
 
     private constructor() {
@@ -147,48 +147,48 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     }
 }
 
-// Project List Class
-class ProjectList  extends Component<HTMLDivElement, HTMLElement>{
+class ProjectList extends Component<HTMLDivElement, HTMLElement> {
+  assignedProjects: Project[];
 
-    assignedProject: Project [];
+  constructor(private type: 'active' | 'finished') {
+    super('project-list', 'app', false, `${type}-projects`);
+    this.assignedProjects = [];
 
-    constructor(private type: 'active' | 'finished') {
-        super('project-list','app',false,`${type}-projects`,);
-        this.assignedProject = [];
-        this.configure();
-        this.renderContent();
-    }
+    this.configure();
+    this.renderContent();
+  }
 
-    configure() {
-        projectState.addListeners((projects: Project[]) => {
-            const relevantProj =projects.filter(prj => {
-                if(this.type ==='active') {
-                    return prj.status === ProjectStatus.Active
-                }
-               return  prj.status === ProjectStatus.Finished
-            })
-
-            this.assignedProject = relevantProj
-            this.renderProject()
-        });
-      }
-
-    private renderProject() {
-        const listEL = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
-        listEL.innerHTML = ''
-        for (const proj of this.assignedProject) {
-            new ProjectItem(this.element.id, proj)
+  configure() {
+    projectState.addListeners((projects: Project[]) => {
+      const relevantProjects = projects.filter(prj => {
+        if (this.type === 'active') {
+          return prj.status === ProjectStatus.Active;
         }
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
+      this.renderProjects();
+    });
+  }
+
+  renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent =
+      this.type.toUpperCase() + ' PROJECTS';
+  }
+
+  private renderProjects() {
+    const listEl = document.getElementById(
+      `${this.type}-projects-list`
+    )! as HTMLUListElement;
+    listEl.innerHTML = '';
+    for (const prjItem of this.assignedProjects) {
+      new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
     }
-
-    renderContent():void {
-        const listId = `${this.type}-projects-list`;
-        this.element.querySelector('ul')!.id = listId;
-        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' Projects';
-    }
-
-
+  }
 }
+
 
 // Project input
 class ProjectInput extends Component<HTMLDivElement, HTMLElement>{;
