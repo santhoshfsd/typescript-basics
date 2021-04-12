a403b353e217584dc7210990429f09de886c7ca6
santhosh raj <santhoshraj19v@gmail.com>
2021-04-12 08:43:25 +0530

    render the app and define the project input class

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
 // Code goes here!
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
+        this.hostElement = document.getElementById('app')! as HTMLDivElement
+
+        //DocumentFragment type
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild! as HTMLFormElement;
+        this.attach();
+    }
+
+    private attach() {
+        // afterbegin afterend  /before
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const project = new ProjectInput();
