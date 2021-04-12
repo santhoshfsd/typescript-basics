30c3ff2a6bb1d23300b45f0882a20d13dfc8cc4f
santhosh raj <santhoshraj19v@gmail.com>
2021-04-12 10:40:15 +0530

    reusable validations

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,47 @@
 
+//interface
+
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number
+}
+
+function validate(validatableInput: Validatable) {
+
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+
+    if (validatableInput.minLength != null && typeof (validatableInput.value) === 'string') {
+        isValid = isValid && validatableInput.minLength < validatableInput.value.trim().length;
+    }
+
+    if (validatableInput.maxLength != null && typeof (validatableInput.value) === 'string') {
+        isValid = isValid && validatableInput.maxLength > validatableInput.value.trim().length;
+    }
+
+    if (validatableInput.min != null && typeof (validatableInput.value) === 'number') {
+        isValid = isValid && validatableInput.value > validatableInput.min
+    }
+
+    if (validatableInput.max != null && typeof (validatableInput.value) === 'number') {
+        isValid = isValid && validatableInput.value < validatableInput.max
+    }
+
+    return isValid;
+}
+
 //decorator
-function autoBind(_:any, _2:string, descriptor:PropertyDescriptor) {
+function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
     const adjDescriptor: PropertyDescriptor = {
-        configurable:true,
-        get(){
+        configurable: true,
+        get() {
             const bound = originalMethod.bind(this);
             return bound
         }
@@ -15,7 +52,7 @@ function autoBind(_:any, _2:string, descriptor:PropertyDescriptor) {
 
 // Project input
 class ProjectInput {
-    
+
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
@@ -30,7 +67,7 @@ class ProjectInput {
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild! as HTMLFormElement;
         this.element.id = "user-input";
-        
+
         this.titleElement = this.element.querySelector('#title') as HTMLInputElement;
         this.descriptionElement = this.element.querySelector('#description') as HTMLInputElement;
         this.ownerElement = this.element.querySelector('#people') as HTMLInputElement;
@@ -43,31 +80,44 @@ class ProjectInput {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 
-    private getUserData():[string, string,number] | void {
+    private getUserData(): [string, string, number] | void {
         const title = this.titleElement.value;
         const description = this.descriptionElement.value;
         const owner = this.ownerElement.value;
-        
-        if(title.trim().length === 0  || description.trim().length === 0 || owner.trim().length === 0) {
+
+        const titleValidatable :Validatable = {
+            value: title, required: true, minLength:5
+        } 
+
+        const descriptionValidatable :Validatable = {
+            value: description, required: true, minLength:5
+        } 
+
+        const ownerValidatable :Validatable = {
+            value: +owner, required: true, min:1, max:5
+        } 
+
+        if(!validate(titleValidatable) || 
+        !validate(descriptionValidatable) || !validate(ownerValidatable)) {
             alert("Invalid Input");
             return;
         }
-        return [title,description,+owner]
+        return [title, description, +owner]
     }
 
 
     @autoBind
-    private submitHandler(event:Event) {
+    private submitHandler(event: Event) {
         event.preventDefault();
         const userInput = this.getUserData();
-        if(Array.isArray(userInput)) {
-            const [title, description, owner] =userInput
+        if (Array.isArray(userInput)) {
+            const [title, description, owner] = userInput
             console.log(title);
             console.log(description);
             console.log(owner);
-            
+
         }
-        
+
     }
 
     private configure() {
